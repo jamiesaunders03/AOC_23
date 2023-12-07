f17f7653d302ac56ce39460140a305b7aa275255
jamie <jamiesaunders03@gmail.com>
2023-12-07 12:57:07 +0000

    Add structure for joker card Need refactor and implementation

--- AOC_23/Challenges/Day7.cs
+++ AOC_23/Challenges/Day7.cs
@@ -11,18 +11,21 @@ namespace AOC_23.Challenges
             public string Hand { get; }
             public int Value { get; }
             public int CardType { get; }
+            public int CardTypeWithJoker { get; }
 
             public CardScores(string hand, int value)
             {
                 Hand = hand;
                 Value = value;
                 CardType = GetCardType(hand);
+                CardType = GetCardTypeWithJoker(hand);
             }
         }
 
         public int Day => 7;
 
         private const string CARD_ORDER = "AKQJT98765432";
+        private const string CARD_ORDER_WITH_JOKER = "AKQT98765432J";
         private static  int _nCardValues = CARD_ORDER.Length;
         private static readonly Regex _cardRe = new(@"(\w+) (\d+)");
         private CardScores[] _cardScores;
@@ -60,7 +63,18 @@ namespace AOC_23.Challenges
 
         public string Challenge2()
         {
-            throw new NotImplementedException();
+            List<CardScores> cards = new(_cardScores);
+            cards.Sort(CompareWithJoker);
+
+            int multiplier = 1;
+            long sum = 0;
+            foreach (CardScores card in cards)
+            {
+                sum += card.Value * multiplier;
+                ++multiplier;
+            }
+
+            return sum.ToString();
         }
 
         private int Compare(CardScores a, CardScores b)
@@ -85,7 +99,49 @@ namespace AOC_23.Challenges
             throw new Exception();
         }
 
+        private int CompareWithJoker(CardScores a, CardScores b)
+        {
+            if (a.Equals(b))
+                return 0;
+
+            int compare = a.CardTypeWithJoker.CompareTo(b.CardTypeWithJoker);
+            if (compare != 0)
+                return compare;
+
+            for (int i = 0; i < a.Hand.Length; ++i)
+            {
+                int val1 = CARD_ORDER.IndexOf(a.Hand[i]);
+                int val2 = CARD_ORDER.IndexOf(b.Hand[i]);
+
+                compare = -val1.CompareTo(val2);
+                if (compare != 0)
+                    return compare;
+            }
+
+            throw new Exception();
+        }
+
         private static int GetCardType(string card)
+        {
+            List<int> appearances = new List<int>(new int[_nCardValues]);
+            foreach (char c in card)
+                appearances[CARD_ORDER.IndexOf(c)] += 1;
+
+            appearances.Sort();
+
+            if (appearances[^1] == 5)
+                return 7;
+            else if (appearances[^1] == 4)
+                return 6;
+            else if (appearances[^1] == 3)
+                return appearances[^2] == 2 ? 5 : 4;
+            else if (appearances[^1] == 2)
+                return appearances[^2] == 2 ? 3 : 2;
+            else
+                return 1;
+        }
+
+        private static int GetCardTypeWithJoker(string card)
         {
             List<int> appearances = new List<int>(new int[_nCardValues]);
             foreach (char c in card)
