ef2d56e59d73437e0866249f7baf6560dfc3c00c
jamie saunders <jamiesaunders03@gmail.com>
2023-12-11 12:50:26 +0000

    Fix logging

--- AOC_23/Program.cs
+++ AOC_23/Program.cs
@@ -5,6 +5,8 @@ using log4net;
 using AocHelper;
 using AOC_23.Challenges;
 
+log4net.Config.XmlConfigurator.Configure();
+
 ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 logger.Info("Starting challenge runner");
 
