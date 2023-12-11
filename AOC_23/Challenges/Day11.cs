cbbee473568ee693d572eeb441ae100669356867
jamie saunders <jamiesaunders03@gmail.com>
2023-12-11 01:42:51 +0000

    Create class for day 11

---
+++ AOC_23/Challenges/Day11.cs
@@ -0,0 +1,24 @@
+﻿using AocHelper;
+
+namespace AOC_23.Challenges
+{
+    internal class Day11 : IAocChallenge
+    {
+        public int Day => 11;
+
+        public Day11()
+        {
+            string[] input = new FetchData(Day).ReadInput().TrimEnd().Split('\n');
+        }
+
+        public string Challenge1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Challenge2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
