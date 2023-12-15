0c761a328272aea55445a864585a483a47f4c109
jamie saunders <jamiesaunders03@gmail.com>
2023-12-15 11:51:39 +0000

    Implement day 15, needs refactor

---
+++ AOC_23/Challenges/Day15.cs
@@ -0,0 +1,112 @@
+﻿using System.Text.RegularExpressions;
+using AocHelper;
+
+namespace AOC_23.Challenges
+{
+    internal class Day15 : IAocChallenge
+    {
+        internal struct Lens
+        {
+            public string Code { get; }
+            public int LensStrength { get; }
+
+            public Lens(string code, int lensStrength)
+            {
+                Code = code;
+                LensStrength = lensStrength;
+            }
+        }
+
+        public int Day => 15;
+
+        private const int MOD = 256;
+        private static readonly Regex _labelRe = new(@"(\w+)(.*)");
+
+        private readonly string[] _commands;
+
+        public Day15()
+        {
+            _commands = new FetchData(Day).ReadInput().TrimEnd().Split(',');
+        }
+
+        public string Challenge1()
+        {
+            int total = _commands.Sum(GetHashScore);
+            return total.ToString();
+        }
+
+        public string Challenge2()
+        {
+            var lens = new List<Lens>[MOD];
+            for (int i = 0; i < MOD; i++)
+            {
+                lens[i] = new List<Lens>();
+            }
+
+            foreach (string s in _commands)
+            {
+                UpdateLens(s, ref lens);
+            }
+
+            var x = lens.SelectMany(s => s.Select(l => l.LensStrength)).ToHashSet();
+
+            long score = 0;
+            for (int box = 0; box < MOD; ++box)
+            {
+                for (int pos = 0; pos < lens[box].Count; ++pos)
+                    score += (box + 1) * lens[box][pos].LensStrength * (pos + 1);
+            }
+
+            return score.ToString();  // 100911 too low
+        }
+
+        private static int GetHashScore(string s)
+        {
+            int hash = 0;
+
+            foreach (char c in s)
+            {
+                hash += c;
+                hash *= 17;
+                hash %= MOD;
+            }
+
+            return hash;
+        }
+
+        private static void UpdateLens(string s, ref List<Lens>[] lens)
+        {
+            Match m = _labelRe.Match(s);
+            string name = m.Groups[1].Value;
+            int index = GetHashScore(name);
+            string rest = m.Groups[2].Value;
+
+            List<Lens> box = lens[index];
+            if (rest == "-")
+            {
+                for (int i = 0; i < box.Count; ++i)
+                {
+                    if (box[i].Code == name)
+                    {
+                        box.RemoveAt(i);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Lens l in box)
+                {
+                    if (l.Code == name)
+                    {
+                        int lensStrength = rest[^1] - '0';
+                        box[box.IndexOf(l)] = new Lens(name, lensStrength);
+                        return;
+                    }
+                }
+
+                box.Add(new Lens(name, rest[^1] - '0'));
+            }
+        }
+    }
+}
