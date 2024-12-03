9b0ec57bd18d92adbc91f486ae851651117c58c5
jamie saunders <jamiesaunders03@gmail.com>
2024-12-03 23:23:28 +0000

    Fix build error

--- AocHelper.Tests/AnswerCache.cs
+++ AocHelper.Tests/AnswerCache.cs
@@ -8,9 +8,9 @@ namespace AocHelper.Tests
         [TestMethod]
         public void TestAddClose()
         {
-            Assert.IsTrue(StoreAnswer.SaveAnswer("123", AnswerState.NOT_QUITE, 0, 0));
-            Assert.IsFalse(StoreAnswer.SaveAnswer("123", AnswerState.NOT_QUITE, 0, 0));
-            Assert.IsTrue(StoreAnswer.SaveAnswer("124", AnswerState.NOT_QUITE, 0, 0));
+            Assert.IsTrue(StoreAnswer.SaveAnswer("123", new Incorrect(), 0, 0));
+            Assert.IsFalse(StoreAnswer.SaveAnswer("123", new Incorrect(), 0, 0));
+            Assert.IsTrue(StoreAnswer.SaveAnswer("124", new Incorrect(), 0, 0));
         }
     }
 }
