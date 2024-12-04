75fca6bb0b94791f91a00e8c4df0f28622ebca9b
jamie saunders <jamiesaunders03@gmail.com>
2024-12-04 21:14:56 +0000

    Create utility for string[] to char[,]

--- AocHelper/Utilities/Extensions.cs
+++ AocHelper/Utilities/Extensions.cs
@@ -12,6 +12,7 @@ namespace AocHelper.Utilities
         /// Populates an array with a value
         /// Note that for reference types this will mean the same reference is used for every element
         /// </summary>
+        /// <param name="arr">The array to fill</param>
         /// <param name="value">The value to fill the array with</param>
         public static void Fill<T>(this T[] arr, T value)
         {
@@ -33,9 +34,33 @@ namespace AocHelper.Utilities
         }
 
         /// <summary>
+        /// Converts an array of string to a 2D grid of chars.
+        /// All strings must be the same length, otherwise a
+        /// </summary>
+        /// <param name="strs">Array of strings to create a grid from</param>
+        /// <returns>2d array of chars, constructed from the original string array</returns>
+        public static char[,] ToGrid(this string[] strs)
+        {
+            if (strs.Length == 0)
+                return new char[,] { };
+
+            int len = strs[0].Length;
+            if (strs.Skip(1).Any(str => str.Length != len))
+                throw new ArgumentException("Strings were not all the same length");
+
+            char[,] grid = new char[strs.Length,len];
+            for (int strIndex = 0; strIndex < strs.Length; ++strIndex)
+                for (int charIndex = 0; charIndex < len; ++charIndex) 
+                    grid[strIndex, charIndex] = strs[strIndex][charIndex];
+
+            return grid;
+        }
+
+        /// <summary>
         /// Populates an array with a value
         /// Note that for reference types this will mean the same reference is used for every element
         /// </summary>
+        /// <param name="arr">The array to fill</param>
         /// <param name="value">The value to fill the array with</param>
         public static void Fill<T>(this T[,] arr, T value)
         {
@@ -49,6 +74,7 @@ namespace AocHelper.Utilities
         /// Populates an array with a value
         /// Note that for reference types this will mean the same reference is used for every element
         /// </summary>
+        /// <param name="arr">The array to fill</param>
         /// <param name="value">The value to fill the array with</param>
         public static void Fill<T>(this T[,,] arr, T value)
         {
