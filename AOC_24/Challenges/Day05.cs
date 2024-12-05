464e7f4573721848444ed168244cdf0a1e4d0941
jamie saunders <jamiesaunders03@gmail.com>
2024-12-05 12:20:59 +0000

    Incomplete day 5 attempt

--- AOC_24/Challenges/Day05.cs
+++ AOC_24/Challenges/Day05.cs
@@ -6,14 +6,54 @@ internal class Day05 : IAocChallenge
 {
     public int Day => 5;
 
+    private readonly List<(int, int)> _rules;
+    private readonly List<int[]> _pages;
+
     public Day05()
     {
-        string[] input = new FetchData(Day, 2024).ReadInput().TrimEnd().Split('\n');
+        // First item is rules, second is pages
+        string[] input = new FetchData(Day, 2024).ReadInput().TrimEnd().Split("\n\n");
+
+        _rules = new List<(int, int)>();
+        _pages = new List<int[]>();
+        
+        foreach (string line in input[0].Split('\n'))
+        {
+            string[] parts = line.Split('|');
+            _rules.Add((int.Parse(parts[0]), int.Parse(parts[1])));
+        }
+
+        foreach (string line in input[1].Split('\n'))
+        {
+            string[] parts = line.Split(',');
+            _pages.Add(parts.Select(int.Parse).ToArray());
+        }
     }
 
     public string Challenge1()
     {
-        throw new NotImplementedException();
+        int total = 0;
+        
+        foreach (int[] page in _pages)
+        {
+            bool accepted = true;
+            for (int i = 0; i < page.Length; ++i)
+            {
+                foreach (int num in page.Skip(i + 1))
+                {
+                    if (_rules.Contains((num, page[i])))
+                    {
+                        accepted = false;
+                        break;
+                    }
+                }
+            }
+
+            if (accepted)
+                ++total;
+        }
+
+        return total.ToString();
     }
 
     public string Challenge2()
