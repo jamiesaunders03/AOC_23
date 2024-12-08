96094b2a82804314f1b8832a03a785c366bc6e34
jamie saunders <jamiesaunders03@gmail.com>
2024-12-08 23:18:01 +0000

    Day8 part 1 complete

---
+++ AOC_24/Challenges/Day08.cs
@@ -0,0 +1,96 @@
+﻿using AocHelper;
+using AocHelper.DataStructures;
+using AocHelper.Utilities;
+
+namespace AOC_24.Challenges;
+
+internal class Day08 : IAocChallenge
+{
+    public int Day => 8;
+
+    private readonly DefaultDictionary<char, List<Vector2>> _antennae;
+    private readonly int _width;
+    private readonly int _height;
+
+    public Day08()
+    {
+        string[] input = new FetchData(Day, 2024).ReadInput().TrimEnd().Split('\n');
+        // string[] input = File.ReadAllLines("../../../TestCases/day8b.txt");
+        _antennae = new DefaultDictionary<char, List<Vector2>>();
+        _height = input.Length;
+        _width = input[0].Length;
+
+        foreach (((int x, int y), char c) in Enumeration.EnumerateArray(input))
+        {
+            if (c != '.')
+                _antennae[c].Add(new Vector2(x, y));
+        }
+    }
+
+    public string Challenge1()
+    {
+        var points = new HashSet<Vector2>();
+        
+        foreach (List<Vector2> antennae in _antennae.Values)
+        {
+            points.UnionWith(GetAdjacentAntiNodesForSatellites(antennae));
+        }
+
+        return points.Count.ToString();
+    }
+
+    public string Challenge2()
+    {
+        var points = new HashSet<Vector2>();
+        
+        foreach (List<Vector2> antennae in _antennae.Values)
+        {
+            points.UnionWith(GetAllAntiNodesForSatellites(antennae));
+        }
+
+        return points.Count.ToString();
+    }
+    
+    private HashSet<Vector2> GetAdjacentAntiNodesForSatellites(List<Vector2> satellites)
+    {
+        if (satellites.Count == 1)
+            return [];
+        
+        var points = new HashSet<Vector2>();
+        foreach (List<Vector2> pair in Enumeration.Combinations(satellites, 2))
+        {
+            Vector2 delta = pair[1] - pair[0];
+
+            points.Add(pair[0] - delta);
+            points.Add(pair[1] + delta);
+        }
+
+        points = points
+            .Where(p => p is { X: >= 0, Y: >= 0 } && p.X < _width && p.Y < _height)
+            .ToHashSet();
+        
+        return points;
+    }
+    
+    private HashSet<Vector2> GetAllAntiNodesForSatellites(List<Vector2> satellites)
+    {
+        if (satellites.Count == 1)
+            return [];
+        
+        var points = new HashSet<Vector2>();
+        foreach (List<Vector2> pair in Enumeration.Combinations(satellites, 2))
+        {
+            Vector2 delta = pair[1] - pair[0];
+
+            // extend
+            points.Add(pair[0] - delta);
+            points.Add(pair[1] + delta);
+        }
+
+        points = points
+            .Where(p => p is { X: >= 0, Y: >= 0 } && p.X < _width && p.Y < _height)
+            .ToHashSet();
+        
+        return points;
+    }
+}
--- AOC_24/Program.cs
+++ AOC_24/Program.cs
@@ -13,10 +13,11 @@ logger.Info("Starting challenge runner");
 const bool VERBOSE = false;
 Action<string> timeReporter = VERBOSE ? Console.WriteLine : _ => { };
 
-ChallengeRunner.RunChallenge<Day01>(timeReporter);
-ChallengeRunner.RunChallenge<Day02>(timeReporter);
-ChallengeRunner.RunChallenge<Day03>(timeReporter);
-ChallengeRunner.RunChallenge<Day04>(timeReporter);
-ChallengeRunner.RunChallenge<Day05>(timeReporter);
-ChallengeRunner.RunChallenge<Day06>(timeReporter);
-ChallengeRunner.RunChallenge<Day07>(timeReporter);
+// ChallengeRunner.RunChallenge<Day01>(timeReporter);
+// ChallengeRunner.RunChallenge<Day02>(timeReporter);
+// ChallengeRunner.RunChallenge<Day03>(timeReporter);
+// ChallengeRunner.RunChallenge<Day04>(timeReporter);
+// ChallengeRunner.RunChallenge<Day05>(timeReporter);
+// ChallengeRunner.RunChallenge<Day06>(timeReporter);
+// ChallengeRunner.RunChallenge<Day07>(timeReporter);
+ChallengeRunner.RunChallenge<Day08>(timeReporter);
