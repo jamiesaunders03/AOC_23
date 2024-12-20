a0b0b9a237059a631f7e6abde62f206fb42b6847
jamie saunders <jamiesaunders03@gmail.com>
2024-12-20 18:56:01 +0000

    Add basic bfs to path finding algos

--- AocHelper/Algorithms/PathFinding.cs
+++ AocHelper/Algorithms/PathFinding.cs
@@ -1,10 +1,50 @@
-﻿using System.Collections;
-using AocHelper.DataStructures;
+﻿using AocHelper.DataStructures;
 
 namespace AocHelper.Algorithms;
 
 public static class PathFinding
 {
+    /// <summary>
+    /// Given a 2D array representing the available paths (true is a path, false is not) returns an array representing
+    /// the distance to each point from the start position, stopping at the end position.
+    /// </summary>
+    /// <param name="maze">The paths that can be taken through the maze</param>
+    /// <param name="start">The start position to search from</param>
+    /// <param name="end">The target position to find the distance to</param>
+    /// <returns></returns>
+    public static int[,] PathFind(bool[,] maze, Vector2 start, Vector2 end)
+    {
+        int[,] map = Utilities.Utilities.Initialize(int.MaxValue, maze.GetLength(0), maze.GetLength(1));
+        
+        Queue<Vector2> queue = new();
+        queue.Enqueue(start);
+        Utilities.Utilities.SetVectorIndex(map, start, 0);
+        
+        while (queue.Count != 0)
+        {
+            Vector2 pos = queue.Dequeue();
+            if (pos == end) 
+                continue;
+            
+            int currentPosVal = Utilities.Utilities.VectorIndex(map, pos);
+            foreach (Vector2 adj in pos.Adjacent())
+            {
+                // if valid next path
+                if (Utilities.Utilities.TryVectorIndex(maze, adj, out bool valid) && valid)
+                {
+                    // if moving would improve score
+                    if (currentPosVal + 1 < Utilities.Utilities.VectorIndex(map, adj))
+                    {
+                        Utilities.Utilities.SetVectorIndex(map, adj, currentPosVal + 1);
+                        queue.Enqueue(adj);
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+    
     /// <summary>
     /// Given a maze of adjacency values, computes the shortest path from start to finish for the maze.
     /// A step of 1 between positions is assumed. The returned collection represents each position of the route
