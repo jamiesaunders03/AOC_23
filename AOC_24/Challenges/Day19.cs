067980220fe39625303349791febb142a5dae67b
jamie saunders <jamiesaunders03@gmail.com>
2024-12-20 18:56:11 +0000

    Day 19 and 20 impl

---
+++ AOC_24/Challenges/Day19.cs
@@ -0,0 +1,58 @@
+﻿using AocHelper;
+
+namespace AOC_24.Challenges;
+
+internal class Day19 : IAocChallenge
+{
+    public int Day => 19;
+
+    private readonly List<string> _cloths;
+    private readonly string[] _targets;
+
+    public Day19()
+    {
+        string[] input = new FetchData(Day, 2024).ReadInput().TrimEnd().Split('\n');
+        
+        _cloths = input[0].Split(", ").ToList();
+        _cloths.Sort((first, second) => first.Length.CompareTo(second.Length));
+        _cloths.Reverse();
+        
+        _targets = input.Skip(2).ToArray();
+    }
+
+    public string Challenge1()
+    {
+        int total = _targets.Count(t => TowelCombinations(t) != 0);
+        return total.ToString();
+    }
+
+    public string Challenge2()
+    {
+        long total = _targets.Sum(TowelCombinations);
+        return total.ToString();;
+    }
+
+    private long TowelCombinations(string towel)
+    {
+        List<string> possibleTowels = _cloths.Where(towel.Contains).ToList();
+        Dictionary<string, long> cache = new();
+        long combinations = RecurseTowel(towel, possibleTowels, cache);
+        
+        return combinations;
+    }
+
+    private static long RecurseTowel(string towel, List<string> subTowels, Dictionary<string, long> cache)
+    {
+        if (towel.Length == 0)
+            return 1;
+        if (cache.TryGetValue(towel, out long res))
+            return res;
+
+        long combinations = subTowels
+            .Where(towel.StartsWith)
+            .Sum(pattern => RecurseTowel(string.Concat(towel.Skip(pattern.Length)), subTowels, cache));
+
+        cache[towel] = combinations;
+        return combinations;
+    }
+}
---
+++ AOC_24/Challenges/Day20.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using AocHelper;
+using AocHelper.Algorithms;
+using AocHelper.DataStructures;
+using AocHelper.Utilities;
+
+namespace AOC_24.Challenges;
+
+internal class Day20 : IAocChallenge
+{
+    private const int SKIP_AMOUNT = 100;
+    private const int CHEAT_DIST_PART_1 = 2;
+    private const int CHEAT_DIST_PART_2 = 20;
+    
+    public int Day => 20;
+
+    private readonly bool[,] _maze;
+    private readonly Vector2 _startPos;
+    private readonly Vector2 _endPos;
+
+    public Day20()
+    {
+        string[] input = new FetchData(Day, 2024).ReadInput().TrimEnd().Split('\n');
+        _maze = new bool[input.Length, input[0].Length];
+        
+        foreach (GridPointer<char> gridPtr in Enumeration.EnumerateArray(input))
+        {
+            var indexVector = new Vector2(gridPtr.Pos.Y, gridPtr.Pos.X);
+            Utilities.SetVectorIndex(_maze, indexVector, gridPtr.Value != '#');
+
+            if (gridPtr.Value == 'S')
+                _startPos = indexVector;
+            else if (gridPtr.Value == 'E')
+                _endPos = indexVector;
+        }
+    }
+
+    public string Challenge1()
+    {
+        int[,] grid = PathFinding.PathFind(_maze, _startPos, _endPos);
+        ICollection<Vector2> path = PathFinding.GetShortestPathFromMaze(grid, _startPos, _endPos);
+
+        int bigSkips = GetBigSkips(grid, path, CHEAT_DIST_PART_1);
+        return bigSkips.ToString();
+    }
+
+    public string Challenge2()
+    {
+        int[,] grid = PathFinding.PathFind(_maze, _startPos, _endPos);
+        ICollection<Vector2> path = PathFinding.GetShortestPathFromMaze(grid, _startPos, _endPos);
+
+        int bigSkips = GetBigSkips(grid, path, CHEAT_DIST_PART_2);
+        return bigSkips.ToString();
+    }
+
+    private static int GetBigSkips(int[,] grid, ICollection<Vector2> path, int cheatDist)
+    {
+        int bigSkips = 0;
+        foreach (Vector2 point in path)
+        {
+            int cost = Utilities.VectorIndex(grid, point);
+            foreach (Vector2 near in point.Nearby(cheatDist))
+            {
+                long skipDist = point.Manhattan(near);
+                if (Utilities.TryVectorIndex(grid, near, out int skippedCost) 
+                    && skippedCost != int.MaxValue
+                    && skippedCost - cost - skipDist >= SKIP_AMOUNT)
+                    ++bigSkips;
+            }
+        }
+
+        return bigSkips;
+    }
+}
