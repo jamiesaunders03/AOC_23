454b74f018605c558a2c7a9648ccca036bb7ec1a
jamie saunders <jamiesaunders03@gmail.com>
2025-12-04 23:27:53 +0000

    Create and test new file template

---
+++ AOC_25/Challenges/Day05.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+using AocHelper;
+using log4net;
+
+namespace AOC_25.Challenges;
+
+public class Day05 : IAocChallenge
+{
+    #region Boilerplate
+
+    private static readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    public int Day => 04;
+
+    #endregion
+
+    public Day05()
+    {
+        _logger.Info($"Constructing {GetType().Name} Solver");
+
+        string[] input = new FetchData(Day, 2025).ReadInput().TrimEnd().Split('\n');
+    }
+
+    /// <summary>
+    /// todo
+    /// </summary>
+    public string Challenge1()
+    {
+        _logger.Info($"Starting {GetType().Name} Part 1");
+
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// todo
+    /// </summary>
+    public string Challenge2()
+    {
+        _logger.Info($"Starting {GetType().Name} Part 2");
+
+        throw new NotImplementedException();
+    }
+}
